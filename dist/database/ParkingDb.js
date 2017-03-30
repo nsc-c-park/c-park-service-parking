634fd71447374805a23a6e1a50e9122f11e61837
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2017-03-30 21:31:20 +0200

    Transpiling TypeScript added

--- dist/database/ParkingDb.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const DbConfig_1 = require("../config/DbConfig");
-class ParkingDb extends DbConfig_1.default.bookshelf().Model {
-    get tableName() { return 'parking'; }
-}
-exports.ParkingDb = ParkingDb;
-exports.default = ParkingDb;
