b60f4293a22090c1458f2664009a7ec1e2919e71
Jacek Czastkiewicz <jacek.czastkiewicz@capgemini.com>
2017-04-16 21:05:13 +0200

    adding test framework

---
+++ test/test.test.ts
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+describe('test', () => {
+    it('should return test nothing', () => {
+        const result = 'X';
+        expect(result).to.equal('X');
+    });
+});
